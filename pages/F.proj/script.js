2bc0dbcf68936b647a00d684d4872c5ef5b6e71e
alex23199323 <alexgorgiashvili@gmail.com>
2021-10-10 23:25:47 +0400

    new item view not finished

--- pages/F.proj/script.js
+++ pages/F.proj/script.js
@@ -299,3 +299,76 @@ $(".filter-hide").on("click", function() {
     let counter = $(".count-inp").val(++x)
 
   });
+
+
+
+  // Smartphone  page 
+
+
+  $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    
+  });
+
+ 
+  let price = $(".capdiv").attr("data-price")
+  let redphone = $(".circlediv").eq(4).attr("data-img")
+  
+
+  $(".circlediv").on("click", function(){
+    let img = $(this).attr("data-img")
+     
+    
+    
+    $(".phimg").attr("src",img)
+    let mimg = $(".phimg").attr("src") 
+    $(".circlediv").removeClass("pricebor")
+    $(this).addClass("pricebor") 
+    if(price == "$749.00" && redphone == mimg ){
+        
+      $(".pricep").children().eq(0).addClass("line ")
+      $(".hiddenspan").removeClass("d-none")
+      
+  }else{
+    $(".pricep").children().eq(0).removeClass("line ")
+    $(".hiddenspan").addClass("d-none")
+  } 
+  
+  console.log( redphone == mimg )
+
+
+  })
+ 
+
+  $(".capdiv").on("click", function(){
+    let price = $(this).attr("data-price")
+    let mimg = $(".phimg").attr("src")
+    
+    $(".pricep").children().eq(0).text(price)
+    $(".capdiv").removeClass("pricebor")
+    $(this).addClass("pricebor")
+    if(price == "$799.00"){
+        $(".clrdiv:last-child").addClass("disable")
+        $(".xx-green").removeClass("d-none")
+
+    }else{
+        $(".clrdiv:last-child").removeClass("disable")
+        $(".xx-green").addClass("d-none")
+    }
+    if( redphone == mimg && price == "$749.00"){
+        
+        $(".pricep").children().eq(0).addClass("line ")
+        $(".hiddenspan").removeClass("d-none")
+        
+    }else{
+      $(".pricep").children().eq(0).removeClass("line ")
+      $(".hiddenspan").addClass("d-none")
+    }       
+  
+  })
+  $(".cardiv").click(function (e) { 
+    e.preventDefault();
+    $(".cardiv").removeClass("pricebor")
+    $(this).addClass("pricebor")
+    
+  });
