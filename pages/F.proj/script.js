db4c1b9796dde5bc6cef91c5c2863d5182936d54
alex23199323 <alexgorgiashvili@gmail.com>
2021-10-21 16:16:44 +0400

    added sign form

--- pages/F.proj/script.js
+++ pages/F.proj/script.js
@@ -30,6 +30,43 @@ $(".main-search").click(function (e) {
     
 });
 
+//  Sign in Form
+$(".change-form-btn1 , .more-sign-click").click(function (e) { 
+  e.preventDefault();
+   $(".pos-form-1").animate({"right" : "100%"},100)
+   $(".pos-form-2").animate({"left" : "0"},100)
+   $(".sign-form-main").animate({"height": "260px"},100)
+   $(".forget-pass").hide()
+   $(".faded-p").hide()
+  
+});
+
+$(".change-form-btn").click(function (e) { 
+  e.preventDefault();
+   $(".pos-form-1").animate({"right" : "0%"},100)
+   $(".pos-form-2").animate({"left" : "100%"},100)
+   $(".sign-form-main").animate({"height": "230px"},100)
+   $(".forget-pass").fadeIn(800)
+   $(".faded-p").fadeIn(800)
+  
+});
+
+
+$(".sign-btn").click(function (e) { 
+  e.preventDefault();
+  $(".fade-main-form").fadeIn(300).addClass("sign-cont-effects")
+  
+});
+$(".sign-x-icon").click(function (e) { 
+  e.preventDefault();
+  $(".fade-main-form").hide().removeClass("sign-cont-effects")
+  
+});
+
+
+
+
+
 //  SWipers
 
 
